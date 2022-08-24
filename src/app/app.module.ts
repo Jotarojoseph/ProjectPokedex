16e2e446f8e65d38fc6b0889a5bbf0ccc8aa2c1a
Ygor <ygorpb2@gmail.com>
2022-08-24 15:58:21 -0300

    Update 24/08/2022

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 //Modules
-import { HttpClientModule } from '@angular/common/http'
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { PagesModule } from './pages/pages.module';
 
